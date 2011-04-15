793c27150c87b0098526cf0f32a185d7595ea376
Philip Cali <philip.cali@gmail.com>
2011-04-14 23:33:11 -0500

    Basic cron syntax parsing in place.

--- src/main/scala/dsl.scala
+++ src/main/scala/dsl.scala
@@ -8,19 +8,20 @@ object CronSyntax {
   implicit def string2cron(syntax: String) = new CronSyntax(syntax)
 }
 
-// I'm slowing understanding this, but Now my brain has
-// had enough for one day 
-class CronSyntax(syntax: String) extends RegexParsers {
-  def every = """Every|every""".r ~ dayKey ~ at ~ midnight
+case class Cron(minute: String, hour: String, dmonth: String, month: String, dweek: String) {
+  override def toString = List(minute, hour, dmonth, month, dweek) mkString (" ")
+}
 
+class CronSyntax(syntax: String) extends RegexParsers {
   private def valueMap[A](vals: Set[A]) =
-    vals.map(_ toString) ++ vals.map(_.toString.substring(0,3))
+    vals.map(_ toString) /*++ vals.map(_.toString.substring(0,3))*/
 
   // Mapping month names, with month keyword
   def monthnames = valueMap(Month.values) 
 
   // Mapping day names, with day keyword
   def daynames = valueMap(Day.values)
+  def daynamesShort = daynames.map(_.substring(0,3))
 
   def isWeekend(day: Day.Value) = {
     val name = day.toString
@@ -29,61 +30,138 @@ class CronSyntax(syntax: String) extends RegexParsers {
 
   def isWeekday(day: Day.Value) = !isWeekend(day)
 
-  // General keywords
-  def sp = whiteSpace
+  def field(name: String) = name ^^ (_ => Map(name -> "*"))
+
+  val every = "Every" | "every"
   val other = "other".r
   val lists = "through" | "until" | "to"
-  val minuteKey = "minute".r
-  val minutesKey = "minute".r
-  val dayKey = "day".r
-  val monthKey = "month".r
-  val hourKey = "hour".r
+  val minuteKey = field("minute")
+  val minutesKey = "minutes".r
+  val dayKey = field("day") 
+  val daysKey = "days".r 
+  val monthKey = field("month")
+  val monthsKey = "months".r
+  val hourKey = field("hour")
+  val hoursKey = "hours".r
   val from = "from".r
 
   // Values
   def monthValue = (monthnames).mkString("|").r 
   def dayValue = (daynames).mkString("|").r
   def hourValue = minuteValue 
-  def minuteValue = """\d{1,2}""".r ^^ (_.toInt)
+  def minuteValue = number 
   def timeValue = hourValue~":"~minuteValue ^^ {
-    case hours~":"~minutes => (hours, minutes)
+    case hours~":"~minutes => (hours, if(minutes.startsWith("0")) minutes.substring(1,2) else minutes)
   }
 
   // Descriptors
-  def noon = "noon" ^^ (_ => 12)
-  def midnight = "midnight" ^^ (_ =>  0)
-  val weekday = """weekday""".r
-  val weekend = """weekend""".r 
+  def noon = "noon" ^^ (_ => ("12", "0"))
+  def midnight = "midnight" ^^ (_ => ("0", "0"))
+  def weekday = "weekday" ^^ (_ => "1-5")
+  def weekend = "weekend" ^^ (_ => "0,6")
 
   // Connectors
   val at = "at".r
   val the = "the".r
-  def hourConnector = at ~ (timeValue | midnight | noon)
-  def dayConnector = "on" ~ (dayValue | the ~ (weekend | weekday))
-  def monthConnector = "in" ~ monthValue
-  def connector = hourConnector | dayConnector | monthConnector
+  val number = """\d{1,2}""".r
+  val st = number <~ "st"
+  val nd = number <~ "nd"
+  val th = number <~ "th"
+
+  def otherWhat(key: String) = {
+    other ~> key ^^ {
+      case key => Map(key -> "*", key+"Modifier" -> "/2")
+    }
+  }
+
+  def timesWhat(key: String) = {
+    number ~ (key+"s") ^^ {
+      case times ~ keys => Map(key -> "*", key+"Modifier" -> ("/"+times))
+    }
+  }
+
+  def hourConnector = at ~> (timeValue | midnight | noon) ^^ {
+    case (hours, minutes) => Map("hour" -> hours, "minute" -> minutes)
+  }
+  
+  def numberDayOfMonth = (st | nd | th) <~ "day" ^^ {
+    case number => Map("day" -> number)
+  }
+
+  def dayConnector = "on" ~> (dayValue | the ~> (weekend | weekday)) ^^ {
+    case day => 
+      val found = Day.values.find(_.toString == day) match {
+        case Some(d) => (d.id - 1).toString
+        case None => day
+      }
+      Map("dweek" -> found)
+  }
+
+  def dayOfMonthConnector = "on the" ~> numberDayOfMonth
+
+  def monthConnector = "in" ~> monthValue ^^ {
+    case month => Map("month" -> Month.values.find(_.toString == month).get.id.toString)
+  }
+
+  def connector = hourConnector | dayConnector | monthConnector | dayOfMonthConnector
+  // Connectors can be strung together
+  // ex: at midnight in June on Sonday
+  def connectors = rep(connector)
 
   // Incremental Syntax
-  def minuteIncrement = (minuteKey 
-                       | other ~ sp ~ minuteKey 
-                       | minuteValue ~ sp ~ minutesKey ~ sp ~ at ~ sp ~ hourValue 
-                       | minuteValue ~ sp ~ minutesKey)
+  def minuteIncrement = every ~> (minuteKey | otherWhat("minute") | timesWhat("minute")) 
+
+  def hourIncrement = every ~> (hourKey | otherWhat("hour") | timesWhat("hour"))
+
+  def dayGeneralIncrement = every ~> (dayKey | otherWhat("day") | timesWhat("day")) 
+
+  def dayMIcrement = every ~> numberDayOfMonth
+
+  def dayIncrement = dayGeneralIncrement | dayMIcrement 
+
+  def monthIncrement = every ~> (monthKey | otherWhat("month") | timesWhat("month"))
+
+  def increment = minuteIncrement | hourIncrement | dayIncrement | monthIncrement
+
+  def incrementers = increment ~ connectors ^^ {
+    case incremented ~ values => 
+    val first = incremented.asInstanceOf[Map[String, String]]
+    val second = values.asInstanceOf[List[Map[String,String]]]
+    applyModifers(second.foldLeft(Map[String,String]()) { (acc, m) =>
+      acc ++ m
+    } ++ first)
+  }
+
+  def applyModifers(result: Map[String, String]) = {
+    val (mods, fin) = result.partition(_._1.endsWith("Modifier"))
+    for((k, v) <- fin) yield((k, v + mods.getOrElse(k+"Modifier", "")))
+  }
 
   // Something like this for the finished result:
-  /*
-    def finished = { 
-      val parsed = parsedResult.split("\\|").map(_.split("=")).toMap
-      Cron(
-        parsed.getOrElse("minute", "0"),
-        parsed.getOrElse("hour", "0"),
-        parsed.getOrElse("dMonth", "*"),
-        parsed.getOrElse("month", "*"),
-        parsed.getOrElse("dweek", "*")
-      )
+  def finished = incrementers
 
-      Cron will have object creation validation
-  */
-  def cron = {
-    parseAll(every, syntax) 
+  def cron = cronOption match {
+    case Left(crond) => crond
+    case Right(msg) => error(msg)
+  }
+
+  def crons = cron.toString
+
+  def cronOption = {
+    parseAll(finished, syntax) match {
+      case Success(parsed, _) =>
+        Left(Cron(
+          parsed.getOrElse("minute", "*"),
+          parsed.getOrElse("hour", "*"),
+          parsed.getOrElse("day", "*"),
+          parsed.getOrElse("month", "*"),
+          parsed.getOrElse("dweek", "*")
+        ))
+      case Failure(msg, _) =>
+        Right(msg) 
+      case Error(msg, _) =>
+        Right(msg)
+    }
   }
 }
+
--- src/test/scala/cron.scala
+++ src/test/scala/cron.scala
@@ -7,8 +7,15 @@ import org.scalatest.matchers.ShouldMatchers
 
 class CronTest extends FlatSpec with ShouldMatchers {
   "Cron dialect" should "be parsable" in {
-    val crond = "Every day at midnight".cron
-    println(crond)
-    //crond should be === "0 0 * * *"
+    "every day at midnight".crons should be === "0 0 * * *" 
+    "every 15 minutes at noon".crons should be === "*/15 12 * * *"
+    "every 2nd day in April at 3:30".crons should be === "30 3 2 4 *"
+    "every day on the weekday at 3:30".crons should be === "30 3 * * 1-5"
+    "every 3 days in July on the weekend at 6:57".crons should be === "57 6 */3 7 0,6"
+  }
+
+  "Cron connectors" should "be interchangable" in {
+    "Every other month on the weekday at midnight".crons should be === "0 0 * */2 1-5"
+    "Every other month at midnight on the weekday".crons should be === "0 0 * */2 1-5"
   }
 }
