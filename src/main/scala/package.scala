de3fe6edaaf6f6ccc9a14c2f4b47d8079a4e7551
Pierre Andrews <pierre.andrews@gmail.com>
2013-04-23 15:35:21 -0300

    some documentation

--- src/main/scala/package.scala
+++ src/main/scala/package.scala
@@ -1,5 +1,8 @@
 package cronish
 
+/**
+ * Provides showtcuts for creating tasks and schedules
+ */
 package object dsl {
   implicit def string2cron(syntax: String) = Cronish(syntax)
 
@@ -9,13 +12,44 @@ package object dsl {
 
   implicit def int2stopgap(limit: Int) = new RichLimitInt(limit)
 
+  /**
+   * every hour at 0:00
+   */
   val hourly = "Every hour at 0:00".cron
+
+  /**
+   * every day at midnight
+   */
   val daily = "Every day at midnight".cron
+
+  /**
+   * every month on Sunday at midnight
+   */
   val weekly = "Every month on Sunday at midnight".cron
+
+  /**
+   * every 1st day in every month at midnight
+   */
   val monthly = "Every 1st day in every month at midnight".cron
+
+  /**
+   * every Every year on the 1st day in January at midnight
+   */
   val yearly = "Every year on the 1st day in January at midnight".cron
 
+/**
+ * create a [[CronTask]] from an anonymous function.
+ *
+ * {{
+ *   val payroll = task {
+ *       println("You have just been paid... Finally!")
+ *   }
+ * }}
+ */
   def task[A](action: => A) = new CronTask(action)
 
+  /**
+   * alias for the task function
+   */
   def job[A](action: => A) = task(action)
 }
